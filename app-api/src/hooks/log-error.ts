497c1831474175816b9240b5952c8e68b71102f0
phidangthanh <dtphi.khtn@gmail.com>
2023-07-17 21:02:49 +0700

    DNR-#6 add format logger

--- app-api/src/hooks/log-error.ts
+++ app-api/src/hooks/log-error.ts
@@ -3,8 +3,15 @@ import type { HookContext, NextFunction } from '../declarations'
 import { logger } from '../logger'
 
 export const logError = async (context: HookContext, next: NextFunction) => {
+  const startTime = performance.now()
+  logger.http(`====================== Api Start App: src/hooks/log-error.ts || Time: ${startTime} MS`)
+  logger.info(`'${context.path}' service method '${context.method}'`)
+
   try {
     await next()
+
+    const endTime = (performance.now() - startTime) / 1000
+    logger.info(`>>>>>>>>>>>>>>>>>>>>>> Api End App: src/hooks/log-error.ts || Time: ${endTime.toFixed(2)} SECONDS ]]]]`)
   } catch (error: any) {
     logger.error(error.stack)
 
@@ -12,6 +19,9 @@ export const logError = async (context: HookContext, next: NextFunction) => {
     if (error.data) {
       logger.error('Data: %O', error.data)
     }
+
+    const endTime = (performance.now() - startTime) / 1000
+    logger.info(`>>>>>>>>>>>>>>>>>>>>>> Api Error End App: src/hooks/log-error.ts || Time: ${endTime.toFixed(2)} SECONDS ]]]]`)
 
     throw error
   }
