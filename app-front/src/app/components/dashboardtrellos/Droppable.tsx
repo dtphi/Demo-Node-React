83cd840645518c829c09a38fbf71582c898c8b88
phidangthanh <dtphi.khtn@gmail.com>
2023-07-18 11:40:32 +0700

    DNR-app-front:#11 add drag-drop card page

--- app-front/src/app/components/dashboardtrellos/Droppable.tsx
+++ app-front/src/app/components/dashboardtrellos/Droppable.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {useDroppable} from '@dnd-kit/core';
 
-export function Droppable(props) {
+export function Droppable(props: { children: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | React.PromiseLikeOfReactNode | null | undefined; }) {
   const {isOver, setNodeRef} = useDroppable({
     id: 'droppable',
   });
