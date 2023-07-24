4d598ec4a143dcec0f1678f81f11623ca342a64d
Giang <tony68hcm@gmail.com>
2023-07-24 20:05:59 +0700

    api/docs cho dep

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, swaggerConfig);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api/docs', app, document);
 
   const port = configService.get('PORT') ?? 3000;
 
